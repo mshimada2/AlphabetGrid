d7a887a91be6eeab757040206ff409e53daadbbc
mshimada <mshimada2@mail.csuchico.edu>
2018-04-06 15:39:29 -0700

    final version

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main()
 {
   int letters;
-
+  vector< vector <char> > alphabet;
   do
   {
     cout<<"How many letters should there be per line? ";
@@ -17,6 +17,65 @@ int main()
       cout<<"\nPlease enter a number between 1-26.\n";
     }
   }while( letters < 1 || letters > 26 );
+
+int rows;
+  
+  if( 26 % letters == 0) 
+  {
+    rows = 26/letters;
+
+  }
+
+  else
+  {
+    rows = 26/letters + 1;
+
+  }
+
+//resize alphabet to have letter # of _rows, and _columns_ # of columns
+  alphabet.resize( rows );
+ 
+  for(int row = 0; row < alphabet.size(); row++)
+  {
+    alphabet[row].resize(letters);
+
+  }
+
+char currentLetter = 'A'; 
+  //traverse the matrix and assign it incrementings letters from A to Z
+for(int row = 0; row < alphabet.size(); row++)
+{
+  for(int column = 0; column < alphabet[row].size(); column++)
+  {
+   
+    alphabet[row][column] = currentLetter;
+    currentLetter++; // increment to next letter in the ASCII table 
+   /* if(currentLetter < 'Z') 
+    {
+      currentLetter++;
+    }
+   */
+    if(currentLetter > 'Z')
+    {
+      currentLetter = 'Z';
+    }
+
+  }
+
+
+}
+
+//display matrix
+for(int row = 0; row < alphabet.size(); row++)
+{
+  for(int column = 0; column < alphabet[row].size(); column++)
+  {
+    cout<<alphabet[row][column];
+  }
+  cout<<endl;
+}
+
+
 
   return 0;
 }
